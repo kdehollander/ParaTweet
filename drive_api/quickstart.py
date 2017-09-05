291fab3837a9f4186a4f033bde8a0dcd30042f3c
root <root@ip-172-31-25-145.us-west-1.compute.internal>
2017-09-05 03:17:36 +0000

    updated tabbing

--- drive_api/quickstart.py
+++ drive_api/quickstart.py
@@ -46,25 +46,25 @@ def get_credentials():
    print('Storing credentials to ' + credential_path)
    return credentials
 
-   def main():
-      """Shows basic usage of the Google Drive API.
+def main():
+   """Shows basic usage of the Google Drive API.
 
-         Creates a Google Drive API service object and outputs the names and IDs
-         for up to 10 files.
-      """
-      credentials = get_credentials()
-      http = credentials.authorize(httplib2.Http())
-      service = discovery.build('drive', 'v3', http=http)
+      Creates a Google Drive API service object and outputs the names and IDs
+      for up to 10 files.
+   """
+   credentials = get_credentials()
+   http = credentials.authorize(httplib2.Http())
+   service = discovery.build('drive', 'v3', http=http)
 
-      results = service.files().list(
-         pageSize=10,fields="nextPageToken, files(id, name)").execute()
-      items = results.get('files', [])
-      if not items:
-         print('No files found.')
-      else:
-         print('Files:')
-      for item in items:
-         print('{0} ({1})'.format(item['name'], item['id']))
+   results = service.files().list(
+      pageSize=10,fields="nextPageToken, files(id, name)").execute()
+   items = results.get('files', [])
+   if not items:
+      print('No files found.')
+   else:
+      print('Files:')
+   for item in items:
+      print('{0} ({1})'.format(item['name'], item['id']))
 
 if __name__ == '__main__':
-main()))
+   main()
