aee331482b05b8def9416f86c28c87a1d07e5dad
Keith DeHollander <kdehollander@vpn-campus-221.csuchico.edu>
2017-09-15 09:02:29 -0700

    update. will need to install a lot of modules

---
+++ twitter_api/clean_and_cluster.py
@@ -0,0 +1,121 @@
+import twitter 
+import json 
+import time 
+import urllib.parse
+import atexit
+import sys
+import os
+import re
+import enchant
+from stemming.porter2 import stem
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cluster import KMeans, MiniBatchKMeans
+from sklearn import metrics
+import emoji
+import numpy as np
+import plotly.plotly as py
+import matplotlib.pyplot as plt
+
+def bag_of_words(lst):
+   vectorizer = CountVectorizer()
+   return vectorizer.fit_transform(lst).todense()
+   #print(vectorizer.vocabulary_)
+
+def isEmoji(word):
+   for char in word:
+      if char in emoji.UNICODE_EMOJI:
+         return True
+   return False
+
+def clean_up_text(txt, post):
+   txt = txt.replace('\\n', '')
+   txt = txt.replace('\\"', '')
+   txt = txt.replace('\\n\\n', '')
+   txt = re.sub(r'^\x0020+', '', txt)
+   txt.strip()
+   for word in txt.split():
+      if word == ' ':
+         txt = txt.replace(word, '')
+      for char in word:
+         if char == '\n':
+            txt = txt.replace(char, '')
+         
+      if post == 1:
+         new_word=stem(word)
+         txt = txt.replace(word, new_word)
+      is_emoji=isEmoji(word)
+      #if is_emoji == True:
+         #txt = txt.replace(word, '') 
+   if "#" in txt:
+      txt = re.sub('#[a-zA-Z0-9]+', '', txt)
+   if "&" in txt:
+      txt = re.sub('&[a-z]+;', '', txt)
+   if "@" in txt:
+      txt = re.sub('@[^ ]+', '', txt)
+   # if it is a retweet, remove 'RT ' so it will not mess up calculations later
+   if "RT" in txt:
+      txt = txt.replace('RT ', '')
+   #make token for link
+   if "http" in txt:
+      txt = re.sub('http[^ ]+', '', txt)
+   #print(txt)
+   #print('\n')
+   return txt
+
+def main():
+   t = open('tweets.txt', 'r+')
+   r = open('replies.txt', 'r+')
+   p = open('posts.txt', 'r+')
+
+   tweets = t.read()
+
+   replies = r.read().split('", "')
+   replies[0] = replies[0].replace('[', '')
+   replies[len(replies) - 1] = replies[len(replies) - 1].replace(']', '')
+
+   posts = p.read().split('", "')
+   posts[0] = posts[0].replace('[', '')
+   posts[len(posts) - 1] = posts[len(posts) - 1].replace(']', '')
+
+   deleted_tweets = 0
+
+   need_to_be_deleted_posts = []
+   for i in range((len(posts) - 1)):
+      posts[i] = clean_up_text(posts[i], 1)
+      if not posts[i] or posts[i].isspace() or len(posts[i].split(' ')) <= 1:
+         deleted_tweets = deleted_tweets + 1
+   for inx in need_to_be_deleted_posts:
+      del posts[inx]
+
+
+   need_to_be_deleted_replies = []
+   for i in range((len(replies) - 1)):
+      replies[i] = clean_up_text(replies[i], 0)
+      if not replies[i] or replies[i].isspace() or len(replies[i].split(' ')) <= 1:
+         deleted_tweets = deleted_tweets + 1
+         need_to_be_deleted_replies.append(i)
+   for inx in need_to_be_deleted_replies:
+      del replies[inx]
+
+   print("deleted tweets = " + str(deleted_tweets))
+
+   bow = bag_of_words(posts)
+   num_clusters = int(len(posts) / 3)
+   km = KMeans(n_clusters=num_clusters, init='k-means++', max_iter=num_clusters*100, n_init=25)
+   ret = km.fit(bow)
+   print(km.labels_)
+   plt.hist(km.labels_)
+
+   print(km.cluster_centers_)
+   print(km.inertia_)
+   #for i in range(num_clusters):
+      #print(i)
+      #index = 0
+      #for l in km.labels_:
+         #if l == i:
+            #print('"'+posts[index]+'"')
+         #index = index + 1
+
+
+if __name__ == "__main__":
+   main()
--- twitter_api/stream.py
+++ twitter_api/stream.py
@@ -1,19 +1,26 @@
 import twitter 
 import json 
-import time 
 import urllib.parse
 import atexit
 import sys
 import re
-import enchant
+from stemming.porter2 import stem
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cluster import KMeans, MiniBatchKMeans
+from sklearn import metrics
+import emoji
+
 api = twitter.Api(consumer_key='aCxaOUuI8nVh2Qqp4zy0qakfz',
       consumer_secret='JS9wfzmkwgKGxuRIT6eGGhLVVaAq3qHBBHKwZELFHIp6pfk54o',
       access_token_key='365288422-1WHmtCRrQZaFsS3U3kxqVAWhf2zBukMrgClY5BD1',
       access_token_secret='taf9r7T8LJw8kXlSNvT7trWcvstJ8uRP89sw528hQtx8w')
 tweets_w_responses = {}
+posts = []
+replies = []
 def main():
    num_tweets = 500
-   results = api.GetStreamFilter(track='-filter:links OR retweets', delimited=500)
+   total_tweets = num_tweets
+   results = api.GetStreamFilter(track='-filter:links OR retweets')
    for tweet in results:
       if tweet.get('in_reply_to_status_id', None) == None:
          continue 
@@ -25,11 +32,11 @@ def main():
       if str(tweet.get('lang', '')) != "en":
          continue
       txt = tweet.get('text', '')
-      txt = clean_up_text(txt)
       # if for some reason, you can't get the tweet's text, or txt is not nothing,skip it
       if not txt or txt.isspace():
          continue 
       #print_error(txt)
+      replies.append(txt)
       # if the tweet has not already been input, add
       if str(tweet.get('in_reply_to_status_id', '')) not in tweets_w_responses:
          try:
@@ -38,11 +45,12 @@ def main():
             continue
          else:
             pos_txt = post.text
-            pos_txt = clean_up_text(pos_txt)
             # remove the username in the beginning
-            if not pos_txt or pos_txt.isspace() or len(pos_txt.split(' ')) <= 2:
+            if not pos_txt or pos_txt.isspace() or len(pos_txt.split(' ')) <= 1:
+               #print("failed")
+               #print(pos_txt)
                continue 
-            print(pos_txt)
+            posts.append(pos_txt)
             tweets_w_responses[str(tweet.get('in_reply_to_status_id', ''))] = {
                'text': pos_txt,
                'user': post.user.screen_name,
@@ -55,59 +63,26 @@ def main():
          })
       num_tweets = num_tweets - 1
 
-def print_error(s):
-   print(s, file=sys.stderr)
+@atexit.register
+def exit():
+   print(posts)
+   with open('tweets.txt', 'w') as t:
+      json.dump(tweets_w_responses, t, ensure_ascii=False)
 
-def clean_up_text(txt):
-   #print(txt)
-   #remove any special chars, emojis, and numbers
-   txt = re.sub('[^A-Za-z0-9\' ]+', '', txt)
-   d = enchant.Dict('en_US')
-   for word in txt.split():
-      is_wrd = d.check(word) 
-      if is_wrd == False:
-         txt = txt.replace(word + ' ', '') 
-   # if it is a retweet, remove 'RT ' so it will not mess up calculations later
-   if "RT" in txt:
-      txt = txt.replace('RT ', '')
-   #remove any links in the text
-   if "http" in txt:
-      txt = re.sub('https[A-Za-z0-9./A-Za-z0-9]+', '', txt)
-   #print(txt)
-   #print('\n')
-   return txt
+   with open('posts.txt', 'w') as p:
+      json.dump(posts, p, ensure_ascii=False)
 
-#@atexit.register
-#def exit():
-   #print(tweets_w_responses)
+   with open('replies.txt', 'w') as r:
+      json.dump(replies, r, ensure_ascii=False)
 
-def get_limit():
-   api_init()
-   rate = api.rate_limit.get_limit('/search/tweets')
-   rem = rate.remaining - 25
-   if rem <= 0:
-      rem - rate.remaining
-   return rem
-
-def get_reset():
-   api_init()
-   rate = api.rate_limit.get_limit('/search/tweets')
-   return rate.reset + 5
-
-def api_init():
-   while True:
-      try:
-         api.InitializeRateLimit()
-      except:
-         print_error("Init Sleeping")
-         time.sleep(60) 
-         continue
-      else:
-         break
+   t.close()
+   p.close()
+   r.close()
 
 if __name__ == "__main__":
-   api_init()
-   print_error("limit: " + str(get_limit()))
-   t_n = time.time()
-   print_error("reset: " + str(get_reset()/60))
+   #api_init()
+   api.InitializeRateLimit()
+   rate = api.rate_limit.get_limit('/search/tweets')
+   #print_error("limit: " + str(rate.remaining - 10))
+   #print_error("reset: " + str((rate.reset+5)/60))
    main()
