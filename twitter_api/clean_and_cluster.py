a4b3bdbc146b712c201a218abcfc4a1ce232a19e
kdehollander <kdehollander@mail.csuchico.edu>
2017-09-24 10:38:20 -0700

    update

--- twitter_api/clean_and_cluster.py
+++ twitter_api/clean_and_cluster.py
@@ -6,22 +6,27 @@ import atexit
 import sys
 import os
 import re
-import enchant
 from stemming.porter2 import stem
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cluster import KMeans, MiniBatchKMeans
 from sklearn.decomposition import PCA
+from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
+from scipy.optimize import minimize, differential_evolution
+from scipy.misc import derivative
 import emoji
 import numpy as np
 import plotly.plotly as py
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-def bag_of_words(lst):
-   vectorizer = CountVectorizer()
-   return vectorizer.fit_transform(lst).todense()
-   #print(vectorizer.vocabulary_)
-
+def max_sil_score(num_clusters, reduced_data):
+   km = MiniBatchKMeans(n_clusters=int(num_clusters), init='k-means++')
+   if len(np.shape(reduced_data)) > 2:
+      return 1
+   ret = km.fit(reduced_data)
+   score = metrics.calinski_harabaz_score(reduced_data, km.labels_)
+   return -score
+   
 def isEmoji(word):
    for char in word:
       if char in emoji.UNICODE_EMOJI:
@@ -35,10 +40,8 @@ def clean_up_text(txt, post):
    txt = re.sub(r'^\x0020+', '', txt)
    txt.strip()
    for word in txt.split():
-      if word == ' ':
-         txt = txt.replace(word, '')
       for char in word:
-         if char == '\n':
+         if char == '\n' or char == ' ':
             txt = txt.replace(char, '')
          
       if post == 1:
@@ -64,19 +67,20 @@ def clean_up_text(txt, post):
    return txt
 
 def main():
-   t = open('tweets.txt', 'r+')
-   r = open('replies.txt', 'r+')
+   #t = open('tweets.txt', 'r+')
+   #r = open('replies.txt', 'r+')
    p = open('posts.txt', 'r+')
 
-   tweets = t.read()
+   #tweets = t.read()
 
-   replies = r.read().split('", "')
-   replies[0] = replies[0].replace('[', '')
-   replies[len(replies) - 1] = replies[len(replies) - 1].replace(']', '')
+   #replies = r.read().split('", "')
+   #replies[0] = replies[0].replace('[', '')
+   #replies[len(replies) - 1] = replies[len(replies) - 1].replace(']', '')
 
    posts = p.read().split('", "')
    posts[0] = posts[0].replace('[', '')
    posts[len(posts) - 1] = posts[len(posts) - 1].replace(']', '')
+   print(len(posts))
 
    deleted_tweets = 0
 
@@ -85,37 +89,123 @@ def main():
       posts[i] = clean_up_text(posts[i], 1)
       if not posts[i] or posts[i].isspace() or len(posts[i].split(' ')) <= 1:
          deleted_tweets = deleted_tweets + 1
+         need_to_be_deleted_posts.insert(0, i)
    for inx in need_to_be_deleted_posts:
       del posts[inx]
+   print(len(posts))
 
-
-   need_to_be_deleted_replies = []
-   for i in range((len(replies) - 1)):
-      replies[i] = clean_up_text(replies[i], 0)
-      if not replies[i] or replies[i].isspace() or len(replies[i].split(' ')) <= 1:
-         deleted_tweets = deleted_tweets + 1
-         need_to_be_deleted_replies.append(i)
-   for inx in need_to_be_deleted_replies:
-      del replies[inx]
+   #need_to_be_deleted_replies = []
+   #for i in range((len(replies) - 1)):
+      #replies[i] = clean_up_text(replies[i], 0)
+      #if not replies[i] or replies[i].isspace() or len(replies[i].split(' ')) <= 1:
+         #deleted_tweets = deleted_tweets + 1
+         #need_to_be_deleted_replies.insert(0, i)
+   #for inx in need_to_be_deleted_replies:
+      #del replies[inx]
 
    print("deleted tweets = " + str(deleted_tweets))
 
-   bow = bag_of_words(posts)
-   reduced_data = PCA(n_components=2).fit_transform(bow)
-   num_clusters = int(len(posts) / 15)
-   km = KMeans(n_clusters=num_clusters, init='k-means++', max_iter=num_clusters*1000, n_init=num_clusters*100)
-   ret = km.fit(reduced_data)
+   print("Bag of words")
+   vectorizer = CountVectorizer()
+   bow = vectorizer.fit_transform(posts).todense()
+   print(bow)
+   print("PCA")
+   print("0 3999")
+   reduced_data = PCA(n_components=2).fit_transform(bow[0:3999])
+   print(reduced_data)
+   rounds = int((len(posts) / 4000)) + 1
+   for i in range(1, rounds):
+      first = (i*4000)
+      last = (((i+1) * 4000) - 1)
+      if last > (len(posts) - 1):
+         last = len(posts) - 1
+      print(str(first) + " " + str(last))
+      pca = PCA(n_components=2).fit_transform(bow[first:last])
+      reduced_data = np.concatenate((reduced_data, pca))
 
-   print(km.labels_)
-   print(km.cluster_centers_)
-   print(km.inertia_)
-   for i in range(num_clusters):
-      print(i)
-      index = 0
-      for l in km.labels_:
-         if l == i:
-            print('"'+posts[index]+'"')
-         index = index + 1
+   #print("Maximize Silhouette")
+   #s_scores = []
+   #ch_scores = []
+   #in_scores = []
+   #for i in range(3, 50):
+      #km = MiniBatchKMeans(n_clusters=i, init='k-means++')
+      #ret = km.fit(reduced_data)
+      #s_scores.append(metrics.silhouette_score(reduced_data, km.labels_))
+      #ch_scores.append(metrics.calinski_harabaz_score(reduced_data, km.labels_) / 5000)
+      #in_scores.append(km.inertia_ / 1000)
+      #if i % 10 == 0:
+         #print(i)
+
+   print("Maximing CH Score")
+   opt = differential_evolution(max_sil_score, bounds=([(2, 100)]), args=([reduced_data]))
+   print(int(opt['x']))
+
+   km = MiniBatchKMeans(n_clusters=int(opt['x']), init='k-means++')
+   ret = km.fit(reduced_data)
+      
+   #print("Starting Bayes")
+   #clf = GaussianNB()
+   #clf.fit(reduced_data, km.labels_)
+
+   #i = open('input.txt', 'r')
+   #s = i.read().split('\n')
+   #sc = clean_up_text(s[0], 1)
+   #print(sc)
+   #sl = [sc]
+   #sb = vectorizer.transform(sl).todense()
+   #print(sb)
+   #print("PCA new input")
+   #sr = PCA(n_components=3).fit_transform(sb)
+   #print(sr)
+
+   #print("predict class")
+   #cl = clf.predict(sr)
+   #print(cl)
+
+   #index = 0
+   #reduced_cluster_x = []
+   #reduced_cluster_y = []
+   #for l in km.labels_:
+      #if l == cl:
+         #reduced_cluster_x.append(reduced_data[index][0])
+         #reduced_cluster_y.append(reduced_data[index][1])
+      #index = index + 1
+   #plt.plot(reduced_cluster_x, reduced_cluster_y, 'k.', markersize=2)
+   #plt.plot(sr[0][0], sr[0][1], 'r.', markersize=2)
+   
+   # Step size of the mesh. Decrease to increase the quality of the VQ.
+   h = .02     # point in the mesh [x_min, x_max]x[y_min, y_max].
+
+   # Plot the decision boundary. For that, we will assign a color to each
+   x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+   y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+   xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+   # Obtain labels for each point in mesh. Use last trained model.
+   Z = km.predict(np.c_[xx.ravel(), yy.ravel()])
+
+   # Put the result into a color plot
+   Z = Z.reshape(xx.shape)
+   plt.figure(1)
+   plt.clf()
+   plt.imshow(Z, interpolation='nearest',
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap=plt.cm.Paired,
+           aspect='auto', origin='lower')
+
+   plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+   # Plot the centroids as a white X
+   centroids = km.cluster_centers_
+   plt.scatter(centroids[:, 0], centroids[:, 1],
+            marker='x', s=169, linewidths=3,
+            color='w', zorder=10)
+   plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
+          'Centroids are marked with white cross')
+   plt.xlim(x_min, x_max)
+   plt.ylim(y_min, y_max)
+   plt.xticks(())
+   plt.yticks(())
+   plt.show()
 
 if __name__ == "__main__":
    main()
